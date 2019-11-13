254e9a33109a37c5c4df1f56fcb12c82e5c1908c
P Skolsky <patrickskolskyart@gmail.com>
2019-11-13 13:51:30 -0500

    fixed the customer app, can now continue shopping or stop shopping

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,69 +20,148 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function(err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+    console.log(`
+~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+
+    Welcome to the Green Gatsby, a Premium New Jersey Marijuana Dispensary!
+    
+~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+`)
+        // run the start function after the connection is made to show the inventory and promt the user
     showProducts();
 });
 
 
 
-
-// function which prompts the user for what action they should take
+// function which prompts the user to select an item and enter the number of selected item they wish to purchase
 function promtStart() {
+
     inquirer
         .prompt([{
             name: "itemId",
             type: "input",
-            message: "Enter the id of the item you wish to purchase",
+            message: "Enter the item ID of the Product you wish to purchase: ",
 
         }, {
             name: "custQuantity",
             type: "input",
-            message: "Enter your desired quantity",
+            message: "Enter your desired quantity: ",
 
         }])
         .then(function(answer) {
+            //we set these variables to easily access our customer's choices from the promt
             let item = answer.itemId;
             let quantity = answer.custQuantity;
+            //these variables will help give the order totals to our user and database
             let userCost;
             let updatedQuantity;
+            let prodName;
 
             connection.query("SELECT * FROM products WHERE ?", { item_id: item }, function(err, res) {
-                    if (err) throw err;
+                prodName = res[0].product_name;
+                stock = res[0].stock_quantity;
+                if (err) throw err;
 
-                    if (quantity > res[0].stock_quantity) {
-                        console.log("Insufficient Quantity");
-                    } else {
+                if (quantity > stock) {
+                    //returns a message to the customer if the quantity they want is more than the current stock
+                    console.log(`
+------------------------------
+------------------------------
 
-                        let userCost = quantity * res[0].price;
-                        let updatedQuantity = res[0].stock_quantity - quantity;
+    I'm sorry, we are unable to fufill the amount you request.  
+        You requested ${quantity}, and our store only has ${stock} of ${prodName} in stock!
+        Please enter an amount less than or equal to ${stock} if you wish to purchase ${prodName}!
+    Thank you!
 
-                        console.log(`You purchased ${quantity} of ${res[0].product_name}! Nice! Your total is ${userCost}! `);
+------------------------------
+------------------------------
+`);
+                    //fires the promts again so they can try to make another purchase
+                    showNoTable();
 
-                        connection.query("Update products set ? where ?", [{ stock_quantity: updatedQuantity }, { item_id: item }], function(err, res) {
-                            if (err) throw err;
-                            console.log(`The inventory for ${res[0].product_name} was updated!`);
-                        })
+                } else {
+                    //getting our user's total and updating our inventory
+                    let userCost = quantity * res[0].price;
+                    let updatedQuantity = res[0].stock_quantity - quantity;
+                    console.log(`
+------------------------------                    
+------------------------------
 
+    Thank you for your purchase!
+        Your cart now contains ${quantity} of ${prodName}! Nice! 
+        Your total is ${userCost} dollars!
 
+------------------------------                    
+------------------------------
+`);
 
+                    connection.query("UPDATE products SET ? WHERE ?", [{ stock_quantity: updatedQuantity }, { item_id: item }], function(err, res) {
+                        if (err) throw err;
+                        // console.log(res);
+                        //we dont really need this console.log message for our customers to see
+                        //                         console.log(`
+                        // ------------------------------
+                        // ------------------------------
 
+                        //     The inventory for ${prodName} was succesfully updated!
 
-                    }
-                    connection.end();
+                        // ------------------------------
+                        // ------------------------------`);
+                        continueShopping()
+
+                    })
                 }
-                // based on their answer, either call the bid or the post functions
-
-            )
+                //end our connection to our database server once we are done purchasing
+                // connection.end();
+            })
         });
 }
 
 function showProducts() {
+    //selects everything from our products table, displays it to our user, and then runs our promt function
     connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
         console.table(res);
         promtStart();
 
         // once you have the items, prompt the user for which they'd like to bid on
 
     })
+};
+
+function showNoTable() {
+    //this function regrabs the info from our db, without flooding the CLI with our table again, and asks the user to make another selection
+    //it occured to me after the fact that this is ACTUALLY no good
+    //the user will have had made a purchase, and they need to see the new updated inventory, so reshowing the table is a GOOD thing
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        promtStart();
+
+    })
+
+}
+
+function continueShopping() {
+    //this function is to ask the customer if the want to continue shopping or if they are finished
+    inquirer.prompt({
+            name: "continue",
+            type: "list",
+            message: "Thank you for shopping with the Green Gatsby! Would you like to continue shopping?",
+            choices: ["Yes, please!", "No thanks, I'm good!"]
+        })
+        .then(function(answer) {
+            switch (answer.continue) {
+                case "Yes, please!":
+                    // connection.end();
+                    showProducts();
+                    break;
+                case "No thanks, I'm good!":
+                    connection.end();
+                    console.log(` 
+*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
+     Thanks again for checking us out!  See you soon!             
+~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~`)
+                    break;
+            }
+        })
 };
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,25 +1,36 @@
+-- alter user 'root' @'localhost' identified WITH mysql_native_password BY 'Octopus0687@';
+
+DROP DATABASE IF EXISTS bamazon_db;
+create database bamazon_db;
 use bamazon_db;
 
+drop table if exists products;
+
+
 create table products  (
-item_id INTEGER(100) auto_increment not null,
+item_id INTEGER(11) auto_increment not null,
 product_name VARCHAR(200) NOT NULL,
 department_name VARCHAR(100) NOT NULL,
-price DECIMAL(10,2) not null,
-stock_quantity integer(250) NOT NULL,
+price DECIMAL(5,2) not null,
+stock_quantity integer(5) NOT NULL,
 PRIMARY KEY (item_id)
 
 );
 
+
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("starberry-cough", "plant-material", 10.00, 250),
-("jack-herer", "plant-material", 15.00, 250),
-("sour-diesel", "plant-material", 15.00, 250),
-("blueberry-haze", "plant-material", 10.00, 250),
-("og-kush", "plant-material", 12.50, 250),
-("oregon-og", "plant material", 12.50, 250),
-("grinder-sm", "accessories", 24.95, 20),
-("grinder-lrg", "accessories", 64.95, 40),
-("raw-organic-hemp-papers", "accessories", 3.75, 250),
-("game-dutch-green", "accessories", 1.25, 250),
-("game-dutch-vanilla", "accessories", 1.25, 250),
-("game-dutch-white-grape", "accessories", 1.25, 250);
+VALUES ("Starberry-Cough", "Plant-Material", 10.00, 448),
+("Jack-Herer", "Plant-Material", 15.00, 448),
+("Sour-Diesel", "Plant-Material", 15.00, 448),
+("Blueberry-Haze", "Plant-Material", 10.00, 448),
+("OG-Kush", "Plant-Material", 12.50, 224),
+("Oregon-OG", "Plant Material", 12.50, 224),
+("Grinder-Sm", "Accessories", 24.95, 100),
+("Grinder-Lrg", "Accessories", 64.95, 150),
+("Bic-Lighter-Single", "Accessories", 1.50, 200),
+("Bic-Lighter-3Pack", "Accessories", 3.75, 200),
+("Raw-Organic-Hemp-Papers", "Materials", 3.75, 350),
+("Backwoods-Cigar", "Materials", 1.25, 250),
+("Game-Dutch-Green", "Materials", 1.25, 250),
+("Game-Dutch-Vanilla", "Materials", 1.25, 250),
+("Game-Dutch-White-Grape", "Materials", 1.25, 250); 
